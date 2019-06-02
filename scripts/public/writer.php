08a9ab6f7fb5f5011b5fafc736200a2013af76b7
vi <reducedprocess@gmail.com>
2019-06-02 15:15:12 +0900

    add authentication checks for api

--- scripts/public/writer.php
+++ scripts/public/writer.php
@@ -1,13 +1,20 @@
 <?php
-if ($_POST['request'] === 'write') {
-	if (($_POST['text']) != null) {
+include 'credentials.php';
 
-		$f=fopen('../../stories/stories.json','w');
-		fwrite($f, $_POST['text']);
-		fclose($f);
+$referer = $_SERVER['HTTP_REFERER'];
 
-		echo 'Saved pin.';
-		return;
+if ($referer == $set) {
+	if ($_POST['request'] === 'write') {
+		if (($_POST['text']) != null) {
+			if (($_POST['pass'] != credentials.php))
+
+			$f=fopen('../../stories/stories.json','w');
+			fwrite($f, $_POST['text']);
+			fclose($f);
+
+			echo 'Saved pin.';
+			return;
+		} else echo 'failure';
 	} else echo 'failure';
-} else echo 'failure;';
+} else echo 'failure';
 ?>
