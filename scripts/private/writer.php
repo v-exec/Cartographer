d892d027211aedcfc77ec4d07ab316f2d8799d05
vi <reducedprocess@gmail.com>
2019-06-05 23:55:42 +0900

    remove debugging lines

--- scripts/private/writer.php
+++ scripts/private/writer.php
@@ -4,16 +4,10 @@ include 'crypt.php';
 session_start();
 
 //check tokens
-if (!hash_equals($_SESSION['t'], $_POST['t']) || !password_verify($frontDoor, $_POST['t']) || !hash_equals($_SESSION['k'], $_POST['k']) || !password_verify($backDoor, $_POST['k'])) {
-	echo 'improper token';
-	return;
-}
+if (!hash_equals($_SESSION['t'], $_POST['t']) || !password_verify($frontDoor, $_POST['t']) || !hash_equals($_SESSION['k'], $_POST['k']) || !password_verify($backDoor, $_POST['k'])) return;
 
 //check for text
-if ($_POST['text'] === null || $_POST['text'] === '') {
-	echo 'missing text';
-	return;
-}
+if ($_POST['text'] === null || $_POST['text'] === '') return;
 
 //sanitize
 $uText = $_POST['text'];
@@ -27,48 +21,26 @@ $uX = htmlspecialchars($uX, ENT_QUOTES, 'UTF-8');
 $uY = htmlspecialchars($uY, ENT_QUOTES, 'UTF-8');
 
 //check if text is appropriate length
-if (strlen($uText) > 40) {
-	echo 'text length too long';
-	return;
-}
+if (strlen($uText) > 40) return;
 
 include 'clean.php';
 $clean = clean($uText);
 
 //check if time is formatted properly
 $noon = substr($uTime, strlen($uTime) - 2, strlen($uTime));
-
-if ($noon !== 'am' && $noon !== 'pm') {
-	echo 'time is not properly formatted';
-	return;
-}
+if ($noon !== 'am' && $noon !== 'pm') return;
 
 $nums = substr($uTime, 0, strlen($uTime) - 2);
 $nums = str_replace(':', '', $nums);
 $nums = str_replace('-', '', $nums);
 $nums = str_replace('/', '', $nums);
 $nums = str_replace(' ', '', $nums);
-
-if (!is_numeric($nums)) {
-	echo 'time is not numeric';
-	return;
-}
+if (!is_numeric($nums)) return;
 
 //check if x and y are appropriate numbers
-if (!is_numeric($uX) || !is_numeric($uY)) {
-	echo 'pin location is not numeric';
-	return;
-}
-
-if ($uX < 5 && $uX > -5 && $uY < 5 && $uY > -5) {
-	echo 'pin location too close to origin';
-	return;
-}
-
-if ($uX > 50000 || $uX < -50000 || $uY > 50000 || $uY < -50000) {
-	echo 'pin location too far';
-	return;
-}
+if (!is_numeric($uX) || !is_numeric($uY)) return;
+if ($uX < 5 && $uX > -5 && $uY < 5 && $uY > -5) return;
+if ($uX > 50000 || $uX < -50000 || $uY > 50000 || $uY < -50000) return;
 
 //create json object
 $newObject = (object) [
@@ -101,6 +73,5 @@ if ($clean) {
 	}
 }
 
-echo 'Pin saved.';
 return;
 ?>
